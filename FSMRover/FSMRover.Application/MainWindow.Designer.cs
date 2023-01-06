b02a7e3f8c29e24c78ef4a0d80b9091366ddeb98
newmersedez <trishkk2001@gmail.com>
2023-01-07 01:17:31 +0300

    я убью себя если не сдам

--- FSMRover/FSMRover.Application/MainWindow.Designer.cs
+++ FSMRover/FSMRover.Application/MainWindow.Designer.cs
@@ -29,150 +29,142 @@
         /// </summary>
         private void InitializeComponent()
         {
-			this.components = new System.ComponentModel.Container();
-			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainWindow));
-			this.timer1 = new System.Windows.Forms.Timer(this.components);
-			this.rover = new System.Windows.Forms.PictureBox();
-			this.chargeCounter = new System.Windows.Forms.Label();
-			this.chargeIcon = new System.Windows.Forms.PictureBox();
-			this.memoryIcon = new System.Windows.Forms.PictureBox();
-			this.memoryCounter = new System.Windows.Forms.Label();
-			this.storageButton = new System.Windows.Forms.PictureBox();
-			this.storageCounter = new System.Windows.Forms.Label();
-			this.startButton = new System.Windows.Forms.Button();
-			((System.ComponentModel.ISupportInitialize)(this.rover)).BeginInit();
-			((System.ComponentModel.ISupportInitialize)(this.chargeIcon)).BeginInit();
-			((System.ComponentModel.ISupportInitialize)(this.memoryIcon)).BeginInit();
-			((System.ComponentModel.ISupportInitialize)(this.storageButton)).BeginInit();
-			this.SuspendLayout();
-			// 
-			// timer1
-			// 
-			this.timer1.Interval = 60;
-			this.timer1.Tick += new System.EventHandler(this.TimerTick);
-			// 
-			// rover
-			// 
-			this.rover.BackColor = System.Drawing.Color.Transparent;
-			this.rover.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
-			this.rover.Image = global::WindowsFormsApp1.Properties.Resources.Rover;
-			this.rover.Location = new System.Drawing.Point(160, 279);
-			this.rover.Name = "rover";
-			this.rover.Size = new System.Drawing.Size(82, 78);
-			this.rover.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-			this.rover.TabIndex = 2;
-			this.rover.TabStop = false;
-			// 
-			// chargeCounter
-			// 
-			this.chargeCounter.BackColor = System.Drawing.Color.Transparent;
-			this.chargeCounter.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-			this.chargeCounter.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-			this.chargeCounter.Location = new System.Drawing.Point(710, 16);
-			this.chargeCounter.Name = "chargeCounter";
-			this.chargeCounter.Size = new System.Drawing.Size(30, 30);
-			this.chargeCounter.TabIndex = 3;
-			this.chargeCounter.Text = "10";
-			// 
-			// chargeIcon
-			// 
-			this.chargeIcon.BackColor = System.Drawing.Color.Transparent;
-			this.chargeIcon.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("chargeIcon.BackgroundImage")));
-			this.chargeIcon.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
-			this.chargeIcon.Location = new System.Drawing.Point(673, 12);
-			this.chargeIcon.Name = "chargeIcon";
-			this.chargeIcon.Size = new System.Drawing.Size(30, 30);
-			this.chargeIcon.TabIndex = 7;
-			this.chargeIcon.TabStop = false;
-			// 
-			// memoryIcon
-			// 
-			this.memoryIcon.BackColor = System.Drawing.Color.Transparent;
-			this.memoryIcon.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("memoryIcon.BackgroundImage")));
-			this.memoryIcon.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
-			this.memoryIcon.Location = new System.Drawing.Point(750, 12);
-			this.memoryIcon.Name = "memoryIcon";
-			this.memoryIcon.Size = new System.Drawing.Size(30, 30);
-			this.memoryIcon.TabIndex = 8;
-			this.memoryIcon.TabStop = false;
-			// 
-			// memoryCounter
-			// 
-			this.memoryCounter.BackColor = System.Drawing.Color.Transparent;
-			this.memoryCounter.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-			this.memoryCounter.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-			this.memoryCounter.Location = new System.Drawing.Point(786, 16);
-			this.memoryCounter.Name = "memoryCounter";
-			this.memoryCounter.Size = new System.Drawing.Size(30, 30);
-			this.memoryCounter.TabIndex = 9;
-			this.memoryCounter.Text = "1";
-			// 
-			// storageButton
-			// 
-			this.storageButton.BackColor = System.Drawing.Color.Transparent;
-			this.storageButton.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("storageButton.BackgroundImage")));
-			this.storageButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
-			this.storageButton.Location = new System.Drawing.Point(822, 12);
-			this.storageButton.Name = "storageButton";
-			this.storageButton.Size = new System.Drawing.Size(30, 30);
-			this.storageButton.TabIndex = 10;
-			this.storageButton.TabStop = false;
-			// 
-			// storageCounter
-			// 
-			this.storageCounter.BackColor = System.Drawing.Color.Transparent;
-			this.storageCounter.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-			this.storageCounter.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-			this.storageCounter.Location = new System.Drawing.Point(858, 16);
-			this.storageCounter.Name = "storageCounter";
-			this.storageCounter.Size = new System.Drawing.Size(30, 30);
-			this.storageCounter.TabIndex = 11;
-			this.storageCounter.Text = "1";
-			// 
-			// startButton
-			// 
-			this.startButton.BackColor = System.Drawing.Color.Firebrick;
-			this.startButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-			this.startButton.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-			this.startButton.Location = new System.Drawing.Point(12, 496);
-			this.startButton.Name = "startButton";
-			this.startButton.Size = new System.Drawing.Size(152, 46);
-			this.startButton.TabIndex = 12;
-			this.startButton.Text = "Start";
-			this.startButton.UseVisualStyleBackColor = false;
-			this.startButton.Click += new System.EventHandler(this.StartButtonClicked);
-			// 
-			// MainWindow
-			// 
-			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.BackColor = System.Drawing.Color.DimGray;
-			this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
-			this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
-			this.ClientSize = new System.Drawing.Size(900, 554);
-			this.Controls.Add(this.startButton);
-			this.Controls.Add(this.storageCounter);
-			this.Controls.Add(this.storageButton);
-			this.Controls.Add(this.memoryCounter);
-			this.Controls.Add(this.memoryIcon);
-			this.Controls.Add(this.chargeCounter);
-			this.Controls.Add(this.chargeIcon);
-			this.Controls.Add(this.rover);
-			this.DoubleBuffered = true;
-			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
-			this.Name = "MainWindow";
-			this.Text = "FSM Rover";
-			((System.ComponentModel.ISupportInitialize)(this.rover)).EndInit();
-			((System.ComponentModel.ISupportInitialize)(this.chargeIcon)).EndInit();
-			((System.ComponentModel.ISupportInitialize)(this.memoryIcon)).EndInit();
-			((System.ComponentModel.ISupportInitialize)(this.storageButton)).EndInit();
-			this.ResumeLayout(false);
-
+	        System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainWindow));
+	        this.roverPictureBox = new System.Windows.Forms.PictureBox();
+	        this.chargeCounter = new System.Windows.Forms.Label();
+	        this.chargeIcon = new System.Windows.Forms.PictureBox();
+	        this.memoryIcon = new System.Windows.Forms.PictureBox();
+	        this.memoryCounter = new System.Windows.Forms.Label();
+	        this.storageButton = new System.Windows.Forms.PictureBox();
+	        this.storageCounter = new System.Windows.Forms.Label();
+	        this.startButton = new System.Windows.Forms.Button();
+	        ((System.ComponentModel.ISupportInitialize)(this.roverPictureBox)).BeginInit();
+	        ((System.ComponentModel.ISupportInitialize)(this.chargeIcon)).BeginInit();
+	        ((System.ComponentModel.ISupportInitialize)(this.memoryIcon)).BeginInit();
+	        ((System.ComponentModel.ISupportInitialize)(this.storageButton)).BeginInit();
+	        this.SuspendLayout();
+	        // 
+	        // roverPictureBox
+	        // 
+	        this.roverPictureBox.BackColor = System.Drawing.Color.Transparent;
+	        this.roverPictureBox.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+	        this.roverPictureBox.Image = global::WindowsFormsApp1.Properties.Resources.Rover;
+	        this.roverPictureBox.Location = new System.Drawing.Point(142, 278);
+	        this.roverPictureBox.Name = "roverPictureBox";
+	        this.roverPictureBox.Size = new System.Drawing.Size(82, 78);
+	        this.roverPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+	        this.roverPictureBox.TabIndex = 2;
+	        this.roverPictureBox.TabStop = false;
+	        // 
+	        // chargeCounter
+	        // 
+	        this.chargeCounter.BackColor = System.Drawing.Color.Transparent;
+	        this.chargeCounter.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+	        this.chargeCounter.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+	        this.chargeCounter.Location = new System.Drawing.Point(710, 16);
+	        this.chargeCounter.Name = "chargeCounter";
+	        this.chargeCounter.Size = new System.Drawing.Size(30, 30);
+	        this.chargeCounter.TabIndex = 3;
+	        this.chargeCounter.Text = "10";
+	        // 
+	        // chargeIcon
+	        // 
+	        this.chargeIcon.BackColor = System.Drawing.Color.Transparent;
+	        this.chargeIcon.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("chargeIcon.BackgroundImage")));
+	        this.chargeIcon.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+	        this.chargeIcon.Location = new System.Drawing.Point(673, 12);
+	        this.chargeIcon.Name = "chargeIcon";
+	        this.chargeIcon.Size = new System.Drawing.Size(30, 30);
+	        this.chargeIcon.TabIndex = 7;
+	        this.chargeIcon.TabStop = false;
+	        // 
+	        // memoryIcon
+	        // 
+	        this.memoryIcon.BackColor = System.Drawing.Color.Transparent;
+	        this.memoryIcon.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("memoryIcon.BackgroundImage")));
+	        this.memoryIcon.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+	        this.memoryIcon.Location = new System.Drawing.Point(750, 12);
+	        this.memoryIcon.Name = "memoryIcon";
+	        this.memoryIcon.Size = new System.Drawing.Size(30, 30);
+	        this.memoryIcon.TabIndex = 8;
+	        this.memoryIcon.TabStop = false;
+	        // 
+	        // memoryCounter
+	        // 
+	        this.memoryCounter.BackColor = System.Drawing.Color.Transparent;
+	        this.memoryCounter.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+	        this.memoryCounter.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+	        this.memoryCounter.Location = new System.Drawing.Point(786, 16);
+	        this.memoryCounter.Name = "memoryCounter";
+	        this.memoryCounter.Size = new System.Drawing.Size(30, 30);
+	        this.memoryCounter.TabIndex = 9;
+	        this.memoryCounter.Text = "1";
+	        // 
+	        // storageButton
+	        // 
+	        this.storageButton.BackColor = System.Drawing.Color.Transparent;
+	        this.storageButton.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("storageButton.BackgroundImage")));
+	        this.storageButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+	        this.storageButton.Location = new System.Drawing.Point(822, 12);
+	        this.storageButton.Name = "storageButton";
+	        this.storageButton.Size = new System.Drawing.Size(30, 30);
+	        this.storageButton.TabIndex = 10;
+	        this.storageButton.TabStop = false;
+	        // 
+	        // storageCounter
+	        // 
+	        this.storageCounter.BackColor = System.Drawing.Color.Transparent;
+	        this.storageCounter.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+	        this.storageCounter.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+	        this.storageCounter.Location = new System.Drawing.Point(858, 16);
+	        this.storageCounter.Name = "storageCounter";
+	        this.storageCounter.Size = new System.Drawing.Size(30, 30);
+	        this.storageCounter.TabIndex = 11;
+	        this.storageCounter.Text = "1";
+	        // 
+	        // startButton
+	        // 
+	        this.startButton.BackColor = System.Drawing.Color.Firebrick;
+	        this.startButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+	        this.startButton.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+	        this.startButton.Location = new System.Drawing.Point(12, 496);
+	        this.startButton.Name = "startButton";
+	        this.startButton.Size = new System.Drawing.Size(152, 46);
+	        this.startButton.TabIndex = 12;
+	        this.startButton.Text = "Start";
+	        this.startButton.UseVisualStyleBackColor = false;
+	        this.startButton.Click += new System.EventHandler(this.StartButtonClicked);
+	        // 
+	        // MainWindow
+	        // 
+	        this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+	        this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+	        this.BackColor = System.Drawing.Color.DimGray;
+	        this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+	        this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+	        this.ClientSize = new System.Drawing.Size(900, 554);
+	        this.Controls.Add(this.startButton);
+	        this.Controls.Add(this.storageCounter);
+	        this.Controls.Add(this.storageButton);
+	        this.Controls.Add(this.memoryCounter);
+	        this.Controls.Add(this.memoryIcon);
+	        this.Controls.Add(this.chargeCounter);
+	        this.Controls.Add(this.chargeIcon);
+	        this.Controls.Add(this.roverPictureBox);
+	        this.DoubleBuffered = true;
+	        this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+	        this.Name = "MainWindow";
+	        this.Text = "FSM Rover";
+	        ((System.ComponentModel.ISupportInitialize)(this.roverPictureBox)).EndInit();
+	        ((System.ComponentModel.ISupportInitialize)(this.chargeIcon)).EndInit();
+	        ((System.ComponentModel.ISupportInitialize)(this.memoryIcon)).EndInit();
+	        ((System.ComponentModel.ISupportInitialize)(this.storageButton)).EndInit();
+	        this.ResumeLayout(false);
         }
 
 		#endregion
-		private System.Windows.Forms.Timer timer1;
-		private System.Windows.Forms.PictureBox rover;
+
+		private System.Windows.Forms.PictureBox roverPictureBox;
 		private System.Windows.Forms.Label chargeCounter;
 		private System.Windows.Forms.PictureBox chargeIcon;
 		private System.Windows.Forms.PictureBox memoryIcon;
--- FSMRover/FSMRover.Application/MainWindow.cs
+++ FSMRover/FSMRover.Application/MainWindow.cs
@@ -15,9 +15,12 @@ namespace WindowsFormsApp1
 {
     public partial class MainWindow : Form
     {
+        private static readonly Point StartPoint = new Point(142, 278);
+        private static readonly Point DestinationPoint = new Point(800, 500);
+
         private readonly IRover _rover;
 
-		public MainWindow(IRover rover)
+        public MainWindow(IRover rover)
         {
             _rover = rover ?? throw new ArgumentNullException(nameof(rover));
 
@@ -27,124 +30,31 @@ namespace WindowsFormsApp1
             memoryCounter.Text = _rover.Memory.ToString();
             storageCounter.Text = _rover.Storage.ToString();
 
-            timer1.Interval = 10;
-            timer1.Tick += new EventHandler(TimerTick);
+            // timer1.Interval = 10;
+            // timer1.Tick += new EventHandler(TimerTick);
         }
 
-        async void TimerTick(object sender, EventArgs e)
+        private void StartProcessing()
         {
-            int currentX = rover.Location.X;
-            int currentY = rover.Location.Y;
+            var curX = roverPictureBox.Location.X;
+            var curY = roverPictureBox.Location.Y;
 
+            var velocity = 10;
+            var offsetX = DestinationPoint.X - curX > 0 ? 1 : -1;
+            var offsetY = DestinationPoint.Y - curY > 0 ? 1 : -1;
 
-            var state = _rover.Update();
-
-            if (state == AllStates.DiscoverArea)
-            {
-                var coefX = 1;
-                var coefY = 1;
-
-                var shiftX = 200 - currentX;
-                if (shiftX < 0) coefX = -1;
-
-                var shiftY = 200 - currentY;
-                if (shiftY < 0) coefY = -1;
-
-                if (currentX != 200 && currentY != 200)
-                {
-                    rover.Location = new Point(currentX + 5 * coefX, currentY + 5 * coefY);
-                }
-            }
-            else if (state == AllStates.CollectSoil)
-            {
-                var coefX = 1;
-                var coefY = 1;
-
-                var shiftX = 30 - currentX;
-                if (shiftX < 0) coefX = -1;
-
-                var shiftY = 300 - currentY;
-                if (shiftY < 0) coefY = -1;
-
-                if (currentX != 300 && currentY != 300)
-                {
-                    rover.Location = new Point(currentX + 5 * coefX, currentY + 5 * coefY);
-                }
-            }   
-            else if (state == AllStates.FixModules)
-            {
-                var coefX = 1;
-                var coefY = 1;
-
-                var shiftX = 0 - currentX;
-                if (shiftX < 0) coefX = -1;
-
-                var shiftY = 0 - currentY;
-                if (shiftY < 0) coefY = -1;
-
-                if (currentX != 0 && currentY != 0)
-                {
-                    rover.Location = new Point(currentX + 5 * coefX, currentY + 5 * coefY);
-                }
-            }
-            else if (state == AllStates.ChargeBatteries)
-            {
-                var coefX = 1;
-                var coefY = 1;
-
-                var shiftX = 400 - currentX;
-                if (shiftX < 0) coefX = -1;
-
-                var shiftY = 400 - currentY;
-                if (shiftY < 0) coefY = -1;
-
-                if (currentX != 400 && currentY != 400)
-                {
-                    rover.Location = new Point(currentX + 5 * coefX, currentY + 5 * coefY);
-                }
-            }
-            else if (state == AllStates.MakePhoto)
-            {
-                var coefX = 1;
-                var coefY = 1;
-
-                var shiftX = 500 - currentX;
-                if (shiftX < 0) coefX = -1;
-
-                var shiftY = 0 - currentY;
-                if (shiftY < 0) coefY = -1;
-
-                if (currentX != 50 && currentY != 0)
-                {
-                    rover.Location = new Point(currentX + 5 * coefX, currentY + 5 * coefY);
-                }
-            }
-            else if (state == AllStates.SendData)
-            {
-                var coefX = 1;
-                var coefY = 1;
-
-                var shiftX = 0 - currentX;
-                if (shiftX < 0) coefX = -1;
-
-                var shiftY = 500 - currentY;
-                if (shiftY < 0) coefY = -1;
-
-                if (currentX != 0 && currentY != 500)
-                {
-                    rover.Location = new Point(currentX + 5 * coefX, currentY + 5 * coefY);
-                }
-            }
-
-            chargeCounter.Text = _rover.Battery.ToString();
-            memoryCounter.Text = _rover.Memory.ToString();
-            storageCounter.Text = _rover.Storage.ToString();
+           while (curX != DestinationPoint.X && curY != DestinationPoint.Y) 
+           { 
+               roverPictureBox.Location = new Point(curX + velocity * offsetX, curY + velocity * offsetY); 
+               roverPictureBox.Refresh();
+               roverPictureBox.Invalidate(); 
+           }
         }
-
-		private void StartButtonClicked(object sender, EventArgs e)
-		{
+        
+        private void StartButtonClicked(object sender, EventArgs e)
+        {
             startButton.Enabled = false;
-            timer1.Start();
+            StartProcessing();
         }
 	}
 }
